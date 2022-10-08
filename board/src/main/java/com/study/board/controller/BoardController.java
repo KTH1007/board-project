8985199f20f6301edce1b607bca2fb7233a20db4
KTH1007 <rlaxogjs2684@gmail.com>
2022-10-08 23:18:39 +0900

    CRUD 게시판 따라만들기

---
+++ board/src/main/java/com/study/board/controller/BoardController.java
@@ -0,0 +1,83 @@
+package com.study.board.controller;
+
+import com.study.board.entity.Board;
+import com.study.board.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@Controller
+public class BoardController {
+    @Autowired
+    private BoardService boardService;
+
+    @GetMapping("/board/write")
+    public String boardWriterForm() {
+        return "boardwrite";
+    }
+
+    @PostMapping("/board/writepro")
+    public String boardWritePro(Board board, Model model, MultipartFile file) throws Exception {
+        boardService.write(board, file);
+        model.addAttribute("message", "글 작성이 완료되었습니다.");
+        model.addAttribute("searchUrl", "/board/list"); //redirect와 비슷한 효과
+        return "message";
+    }
+
+    @GetMapping("/board/list")
+    public String boardList(Model model, @PageableDefault(page=0, size = 20, sort="id", direction = Sort.Direction.DESC) Pageable pageable,
+                            String searchKeyword){
+        Page<Board> list = null;
+        if(searchKeyword == null){
+            list = boardService.boardList(pageable);
+        }
+        else{//검색을 한 경우
+            list = boardService.boardSearchList(searchKeyword, pageable);
+        }
+        int nowPage = list.getPageable().getPageNumber() + 1; //0부터 시작하기 때문에 1을 더해줌
+        int startPage = Math.max(nowPage - 4, 1);  //음수가 나오는 것을 방지하기 위해 Math.max를 사용
+        int endPage = Math.min(nowPage + 5, list.getTotalPages());
+        model.addAttribute("list",list);
+        model.addAttribute("nowPage", nowPage);
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
+        return "boardlist";
+    }
+
+    @GetMapping("/board/view")
+    public String boardView(Model model, Integer id){
+        model.addAttribute("board", boardService.boardView(id));
+        return "boardview";
+    }
+
+    @GetMapping("/board/delete")
+    public String boardDelete(Integer id){
+        boardService.boardDelete(id);
+        return "redirect:/board/list";
+    }
+
+    @GetMapping("/board/modify/{id}")
+    public String boardModify(@PathVariable("id") Integer id, Model model){
+        model.addAttribute("board", boardService.boardView(id));
+        return "boardmodify";
+    }
+
+    @PostMapping("/board/update/{id}")
+    public String boardUpdate(@PathVariable("id") Integer id, Board board) throws Exception{
+        Board boardTemp = boardService.boardView(id); //기존에 있던 글이 담겨져서 옴
+        boardTemp.setTitle(board.getTitle());
+        boardTemp.setContent(board.getContent());
+        boardService.write(boardTemp);
+        return "redirect:/board/list";
+    }
+}
